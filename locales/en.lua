fb66f3c9ea9bc65309a0f153b499bf4e50c0f3b3
Lucifer Morningstar <108193124+JustAuramo@users.noreply.github.com>
2024-03-29 04:20:08 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+  ['HandCuff_uncuff'] = 'HandCuff / uncuff',
+  ['FeetCuff_uncuff'] = 'Raudoita',
+  ['Darg'] = 'Siirrä',
+  ['Sechce'] = 'Tutki Taskut',
+  ['PutInVehiclece'] = 'Laita Ajoneuvoon',  
+  ['OutVehiclece'] = 'Ulos Ajoneuvosta',
+  ['StopDargging'] = '[G] - Lopeta Siirtäminen',
+  ['searching'] = 'Tutkitaan...',
+
+
+}
